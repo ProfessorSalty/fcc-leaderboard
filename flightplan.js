b44dad65122b4a226232ea8ca53a0eeaee58511e
Gregory Smith <crashingwaves@fea.st>
2016-10-14 12:11:38 -0600

    Fixes

--- flightplan.js
+++ flightplan.js
@@ -61,7 +61,9 @@ plan.remote(['staging', 'production'], function(remote) {
     remote.exec(`chown -R ${plan.runtime.options.ownerUser}:${plan.runtime.options.group} ${plan.runtime.options.webRoot}/${appName}/${versionedDir}`);
     remote.with(`cd ${plan.runtime.options.webRoot}/apps`, function() {
         remote.log("Linking")
-        local.exec(`rm -r ~/${appName}`);
+        remote.exec(`rm -rf ${appName}`);
         remote.exec(`ln -sf ../${appName}/${versionedDir} ${appName}`);
     });
 });
+
+
